55419a8cc0f481b7bd829864030bef58454cbbf2
user <user@WIN-MHGL1PRC8MH>
2024-06-24 14:02:12 +0300

    1

--- Wind/Form1.cs
+++ Wind/Form1.cs
@@ -23,6 +23,49 @@ namespace Wind
 
         }
 
+
+        private const int radious = 3;
+        private const int numb_point_for_curve = 50;
+
+
+
+        private void Draw(Point[] p)
+        {
+            Graphics g;
+            g = panel1.CreateGraphics();
+            g.Clear(Color.White);
+            Pen pen = new Pen(Color.Red, 2);
+            Pen curve_p = new Pen(Color.Blue, 2);
+
+            foreach (Point point in p)
+            {
+                g.DrawEllipse(pen, point.X - radious, point.Y - radious, 2 * radious, 2 * radious);
+            }
+
+            Point[] curvePoints = new Point[numb_point_for_curve];
+            for (int i = 0; i < numb_point_for_curve; i++)
+            {
+                float t = (float)i / (numb_point_for_curve - 1);
+                curvePoints[i] = cubic_Bezier(t, p[0], p[1], p[2], p[3]);
+            }
+            g.DrawLines(curve_p, curvePoints);
+        }
+
+        private Point cubic_Bezier(float t, Point p0, Point p1, Point p2, Point p3)
+        {
+            float u = 1 - t;
+            float t1 = t * t;
+            float u1 = u * u;
+            float u2 = u1 * u;
+            float t2 = t1 * t;
+
+            int x = (int)(u2 * p0.X + 3 * u1 * t * p1.X + 3 * u * t1 * p2.X + t2 * p3.X);
+            int y = (int)(u2 * p0.Y + 3 * u1 * t * p1.Y + 3 * u * t1 * p2.Y + t2 * p3.Y);
+
+            return new Point(x, y);
+        }
+
+
         private void button1_Click(object sender, EventArgs e)
         {
             Graphics g;
@@ -38,7 +81,10 @@ namespace Wind
 
             Pen pen = new Pen(Color.Black);
 
-            g.DrawBezier(pen, points[0], points[1], points[2], points[3]);
+
+            Draw(points);
+
+            //g.DrawBezier(pen, points[0], points[1], points[2], points[3]);
         }
         private void SierpinskiCarpet(int order, int x, int y, int width, int height)
         {
@@ -80,7 +126,7 @@ namespace Wind
             w = Convert.ToInt32(textBox12.Text);
             order = Convert.ToInt32(textBox13.Text);
 
-            SierpinskiCarpet(order,x,y,w,h);
+            SierpinskiCarpet(order, x, y, w, h);
         }
     }
 }
